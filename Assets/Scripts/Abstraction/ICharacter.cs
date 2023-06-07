0dfc8f96e4f71e1fafce28b2f4971d95eef71fc9
牛奶 <burpee_soggier0x@icloud.com>
2023-06-07 17:57:17 +0800

    初始化提交

---
+++ Assets/Scripts/Abstraction/ICharacter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Abstraction
+{
+    public interface ICharacter
+    {
+        public void Attack(GameObject gameObject);
+    }
+}
---
+++ Assets/Scripts/Framework/ObjectPool.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Framework
+{
+    public class ObjectPool<PoolType>
+    {
+        private const int POOL_DEDAULT_SIZE = 128;
+        private readonly static Dictionary<string, object> Pools = new Dictionary<string, object>();
+        public static ObjectPool<PoolType> CreateNewPool(int poolSize = POOL_DEDAULT_SIZE)
+        {
+            string typeFullName = typeof(PoolType).FullName;
+            if (Pools.TryGetValue(typeFullName, out object objPool))
+            {
+                ObjectPool<PoolType> result = (ObjectPool<PoolType>)objPool;
+                if (poolSize != POOL_DEDAULT_SIZE)
+                    result.ChangePoolSize(poolSize);
+                return result;
+            }
+            ObjectPool<PoolType> pool = new ObjectPool<PoolType>(poolSize);
+            Pools.Add(typeFullName, pool);
+            return pool;
+        }
+
+        private PoolType[] __PoolType;
+
+        private ObjectPool(int poolSize)
+        {
+            __PoolType = new PoolType[poolSize];
+        }
+
+        private void ChangePoolSize(int newSize)
+        {
+            PoolType[] poolTypes = new PoolType[newSize];
+            Array.Copy(__PoolType, poolTypes, __PoolType.Length > poolTypes.Length ? poolTypes.Length : __PoolType.Length);
+            __PoolType = poolTypes;
+        }
+
+        public void Add(PoolType poolType)
+        {
+
+        }
+
+        public PoolType MakeNew()
+        {
+            return Activator.CreateInstance<PoolType>();
+        }
+    }
+}
